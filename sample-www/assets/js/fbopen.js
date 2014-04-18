b21a8978cfcf5c8caf608b5f38d12e89c4295fb8
arowla <alison.rowland@gsa.gov>
2014-04-18 15:57:39 -0400

    Disregard trailing slash in query parameter parsing

--- sample-www/assets/js/fbopen.js
+++ sample-www/assets/js/fbopen.js
@@ -502,11 +502,16 @@
 function getQueryVariable(variable)
 {
        var query = window.location.search.substring(1);
+
+       // don't let trailing slash get appended to the last query param value
+       query = query.replace(/\/$/, "");
+
        var vars = query.split("&");
        for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
+         var pair = vars[i].split("=");
+         if(pair[0] == variable){return pair[1];}
        }
+
        return(false);
 }
 
