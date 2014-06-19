47253a81c547dfab365854686f8c8ea743151f5a
Joshua Ruihley <ruihley@gmail.com>
2014-06-19 14:42:14 -0400

    remove max topic limit and change dodsbirscrape requirements url

--- loaders/dodsbir.net/gettopics.py
+++ loaders/dodsbir.net/gettopics.py
@@ -3,7 +3,7 @@ from dodsbirscrape import DODSBIRScrape
 def save_all():
 	s = DODSBIRScrape()
 	s.stage_current_solicitation()
-	s.get_all_topics(max=3)
+	s.get_all_topics()
 	s.save_as_json("workfiles/alltopics.json")
 
 save_all()
